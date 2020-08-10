6573ccdb8cd823c3a4a8736af0690cc0ae87e9d7
Anton Shtipliyski <antonshtipliyski@yahoo.com>
2020-08-10 12:38:22 -0300

    Updates

---
+++ precache-manifest.2ab86f8f575d547bc39e06dccfa53a08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5271f4df123fdda5e1e2b5607b9f2ca0",
+    "url": "/deploy-app/index.html"
+  },
+  {
+    "revision": "45876584c3b333a8521f",
+    "url": "/deploy-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9840abe94cc7accc3b2e",
+    "url": "/deploy-app/static/js/2.c436efef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-app/static/js/2.c436efef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45876584c3b333a8521f",
+    "url": "/deploy-app/static/js/main.1cb1affd.chunk.js"
+  },
+  {
+    "revision": "04724fc3230f20dc116b",
+    "url": "/deploy-app/static/js/runtime-main.8d774ace.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
