e391acdd9fde4de22b73580bf747652fe6362009
Anton Shtipliyski <antonshtipliyski@yahoo.com>
2020-08-10 13:34:19 -0300

    Updates

--- precache-manifest.a6aeccfb9daf7f63becf5ff0e6dd6f9c.js
+++ precache-manifest.a6aeccfb9daf7f63becf5ff0e6dd6f9c.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "79167ed73c767cbf20c23a3c21b2e4f2",
+    "revision": "b81a21c2378b30359bec5a1b6c5daaa9",
     "url": "/deploy-app/index.html"
   },
   {
-    "revision": "7e11b32e412320a17c99",
-    "url": "/deploy-app/static/css/main.5f361e03.chunk.css"
+    "revision": "7c9bedbd473a2711b0c8",
+    "url": "/deploy-app/static/css/main.ae1d157f.chunk.css"
   },
   {
-    "revision": "b4085bf57e9aa2f43454",
-    "url": "/deploy-app/static/js/2.eb263e82.chunk.js"
+    "revision": "32c95a7904a5db1e3764",
+    "url": "/deploy-app/static/js/2.fa05423c.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/deploy-app/static/js/2.eb263e82.chunk.js.LICENSE.txt"
+    "url": "/deploy-app/static/js/2.fa05423c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7e11b32e412320a17c99",
-    "url": "/deploy-app/static/js/main.b345ccd8.chunk.js"
+    "revision": "7c9bedbd473a2711b0c8",
+    "url": "/deploy-app/static/js/main.b4ba32f1.chunk.js"
   },
   {
     "revision": "04724fc3230f20dc116b",
